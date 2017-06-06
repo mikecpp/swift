ee07f2ace82dca71f7f5958e371d2173199a71d7
Mike Chen <mikecpp.chen@msa.hinet.net>
2017-06-06 08:28:20 +0800

    add arr & str

---
+++ array_01.swift
@@ -0,0 +1,12 @@
+var arr = [0, 1, 2, 3, 4]
+for i in arr {
+    print("\(i) ", terminator: "")
+}
+
+print("\n\(arr.count)")
+
+var fruits = ["Apple", "Orange", "Banana"]
+for food in fruits {
+    print(food)
+}
+
---
+++ str_test.swift
@@ -0,0 +1,4 @@
+var stringValue: String? = "Hello Swift"
+print(stringValue)
+stringValue = nil
+print(stringValue)
